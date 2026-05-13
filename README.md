b854369df7f51f41d1924cb75564221c7f41a43b
Oliver Marquez <omarquez@c2r1s4.42urduliz.com>
2026-05-13 15:44:07 +0200

    added pseudocode in ft_printf.c

---
+++ README.md
@@ -0,0 +1,4 @@
+
+[tutorialspoint]https://www.tutorialspoint.com/c_standard_library/stdarg_h.htm
+[stackoverflow]https://stackoverflow.com/questions/40484293/stdarg-and-printf-in-c
+[geeksforgeeks]https://www.geeksforgeeks.org/c/variadic-functions-in-c/
--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,49 @@
 /*   By: omarquez <omarquez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 11:12:07 by omarquez          #+#    #+#             */
-/*   Updated: 2026/05/12 16:19:39 by omarquez         ###   ########.fr       */
+/*   Updated: 2026/05/13 15:43:29 by omarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
+static void	ft_handle_conversion(char c)
+{
+	if (c == 'c')//show character
+		ft_putchar_fd(c, 1);
+	else if (c == 's')//show string
+		ft_putstr_fd(c, 1);
+	else if (c == 'p')//show void pointer in hexadecimal
+		ft_putchar_fd(c, 1);
+	else if (c == 'd')//show decimal base 10
+		ft_putchar_fd(c, 1);
+	else if (c == 'i')//show integer base 10
+		ft_putchar_fd(c, 1);
+	else if (c == 'u')//show integer base 10 without sign
+		ft_putchar_fd(c, 1);
+	else if (c == 'x')//show hexadecimal base 16 in minus
+		ft_putchar_fd(c, 1);
+	else if (c == 'X')//show hexadecimal base 16 in mayus
+		ft_putchar_fd(c, 1);
+	else if (c == '%')//show percentage
+		ft_putchar_fd(c, 1);
+	
+}
+
 int	ft_printf(char const *str, ...)
 {
+    int counter;
+    
+    counter = 0;
 	if (!str)
 		return (0);
-	if (!ft_strchr(str, '%'))
-		ft_putstr_fd((char *)str, 1);
+	while (str[counter] != '%' && str[counter])
+	{
+	    ft_putchar_fd(str[counter], 1);
+	    counter ++;
+	}
+	if (str[counter] == '%' && str[counter + 1])
+	    ft_handle_conversion(str[counter + 1]);
 	return (0);
 }
 
