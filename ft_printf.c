807481394a0228c3a6063d151f9f78d204206a1f
Oliver Marquez <omarquez@c2r1s4.42urduliz.com>
2026-05-15 11:28:37 +0200

    wip on ft_printf.c, controlling args

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: omarquez <omarquez@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/09 11:12:07 by omarquez          #+#    #+#             */
-/*   Updated: 2026/05/14 13:03:55 by omarquez         ###   ########.fr       */
+/*   Updated: 2026/05/15 11:25:58 by omarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 static void	ft_handle_conversion(char c, va_list list)
 {
-	if (c == 'c')//show character
+	if (c == 'c')//show character (done?)
 		ft_putchar_fd(va_arg(list, int), 1);
-	else if (c == 's')//show string
+	else if (c == 's')//show string (done?)
 		ft_putstr_fd(va_arg(list, char *), 1);
 	else if (c == 'p')//show void pointer in hexadecimal
-		ft_putchar_fd(c, 1);
-	else if (c == 'd')//show decimal base 10
-		ft_putchar_fd(c, 1);
+		ft_putstr_fd(va_arg(list, void *), 1);
+	else if (c == 'd')//show decimal base 10 (done?)
+		ft_putnbr_fd(va_arg(list, int), 1);
 	else if (c == 'i')//show integer base 10
 		ft_putchar_fd(c, 1);
 	else if (c == 'u')//show integer base 10 without sign
@@ -30,32 +30,46 @@ static void	ft_handle_conversion(char c, va_list list)
 		ft_putchar_fd(c, 1);
 	else if (c == 'X')//show hexadecimal base 16 in mayus
 		ft_putchar_fd(c, 1);
-	else if (c == '%')//show percentage
+	else if (c == '%')//show percentage (done)
 		ft_putchar_fd(c, 1);
 }
 
 int	ft_printf(char const *str, ...)
 {
-    int counter;
+    int 	counter;
+    size_t 	str_len;
 	va_list list;
 	va_start(list, str);
 
-    counter = 0;
 	if (!str)
-		return (0);
+	return (0);
+    counter = 0;
+	str_len = ft_strlen(str);
+	if (str[str_len - 1] == '%' && str[str_len] == '\0')
+		return (-1);
 	while (str[counter] != '%' && str[counter])
 	{
 	    ft_putchar_fd(str[counter], 1);
 	    counter ++;
 	}
 	if (str[counter] == '%' && str[counter + 1])
-	    ft_handle_conversion(str[counter + 1], list);
-	return (0);
+		{
+			ft_handle_conversion(str[counter + 1], list);
+			counter += 2;
+		}
+	while (str[counter] != '%' && str[counter])
+	{
+	    ft_putchar_fd(str[counter], 1);
+	    counter ++;
+	}
+	return (counter);
 }
 
 #include <stdio.h>
 int	main(void)
 {
-	ft_printf("this is a string %s", "string");
+	int	result = ft_printf("this is a string %d\n", 1234); 
+	
+	printf("%d characters were written", result);
 	return (0);
 }
